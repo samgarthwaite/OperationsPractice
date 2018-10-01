290ec1753dcf3d852163b36fef2421623fcddd51
samgarthwaite <43002471+samgarthwaite@users.noreply.github.com>
2018-10-01 08:32:51 -0400

    Add files via upload

---
+++ src/OperationTest.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ * @author Sam Garthwaite
+ *
+ */
+public class OperationTest 
+{
+	public static void main(String[] args)
+	{
+		int num1; //Data type - Variable Name
+			num1 = 20;
+		System.out.println("The starting value is \n" + num1);
+		num1 += 5; //Shortcut for num1 = num1 + 5
+		System.out.println("When you add 5 it is\n" + num1);
+		num1 /= 2; //It takes 1 double to make the answer a double
+		System.out.println("when you divide by 2 it is \n" + num1);
+		num1 *= 4;
+		System.out.println("When you multiply by 4 it is \n" + num1);
+		num1 -= 6;
+		System.out.println("When you subtract 6 it is \n" + num1);
+		num1 %= 2;
+		System.out.println("When you modular divide by 2 it is\n" + num1);
+		// Shortcut for thisSystem.out.println();
+		
+		
+	}
+}
+ 
